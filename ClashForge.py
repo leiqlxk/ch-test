c5de1715d9e24d083f91ccdaeb301c53b02547f9
leiqlxk <472990991@qq.com>
2025-04-03 10:05:21 +0800

    Update ClashForge.py

--- ClashForge.py
+++ ClashForge.py
@@ -2442,7 +2442,6 @@ if __name__ == '__main__':
   "https://proxypool.link/trojan/sub",
   "https://proxypool.link/ss/sub|ss",
   "https://proxypool.link/vmess/sub",
-  "https://mxlsub.me/newfull",
-  "https://igdux.top/5Hna",
+  "https://mxlsub.me/newfull"
 ]
     work(links, check=True, only_check=False, allowed_types=["ss","hysteria2","hy2","vless","vmess","trojan"])
