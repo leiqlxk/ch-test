ff7aedecc53fc318d1f59773b24ec9efaa260b67
leiqlxk <472990991@qq.com>
2025-04-14 14:02:00 +0800

    Update ClashForge.py

--- ClashForge.py
+++ ClashForge.py
@@ -2336,8 +2336,6 @@ def work(links,check=False,allowed_types=[],only_check=False):
 
 if __name__ == '__main__':
 #     links = [
-#  "http://1.116.162.163:10090",
-#         "http://dingyue.davesuen.store",
 #         "https://dy.5468936.xyz",
 #         "http://dy.5468936.xyz",
 #         "https://dingyue.davesuen.store",
@@ -2424,6 +2422,8 @@ if __name__ == '__main__':
   "https://sub.reajason.eu.org/clash.yaml",
   "https://proxypool.link/trojan/sub",
   "https://raw.githubusercontent.com/midpoint/pools2/refs/heads/master/Eternity.yml",
-  "http://103.67.52.65"
+  "http://103.67.52.65",
+  "http://1.116.162.163:10090",
+  "http://dingyue.davesuen.store"
 ]
     work(links, check=True, only_check=False, allowed_types=["ss","hysteria2","hy2","vless","vmess","trojan"])
